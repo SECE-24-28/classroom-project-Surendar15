7b4e704f7ff10ed8e0f9b8090f7bf27e57729f5d
Surendra Prakash <akilantheunique16@gmail.com>
2025-12-05 08:47:11 +0530

    Add files via upload

---
+++ day4/ar1.js
@@ -0,0 +1,21 @@
+let arr=["arun","balu",".xyz"]
+console.log(arr)
+console.log(arr.toString())
+arr.push("arul")
+console.log(arr)
+arr.unshift("Vimal")
+console.log(arr)
+arr.pop()
+console.log(arr)
+arr.shift()
+console.log(arr)
+let arr1=arr.splice(1,3)
+console.log(arr1)
+let arr2=arr.splice(0)
+console.log(arr2)
+delete arr2[0]
+console.log(arr2)
+ 
+var x=100;
+var x=200;
+console.log(x);
